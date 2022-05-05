73b0f3b0b88bfc0f1e7417933d7ec926ed756c3c
IlyaEmeliyanov <ilya.emeliyanov20@gmail.com>
2022-05-05 11:16:45 +0200

    Initial Commit

---
+++ GradientMatcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradientMatcher
+//
+//  Created by Ilya Emeliyanov on 05/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GradientMatcher/GradientMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientMatcherApp.swift
+//  GradientMatcher
+//
+//  Created by Ilya Emeliyanov on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct GradientMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
